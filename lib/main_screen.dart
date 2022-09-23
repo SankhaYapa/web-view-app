97e46226d8c447a382093f7d78491c569ff8baa1
SankhaYapa <sankhapriyamantha55@gmail.com>
2022-09-23 21:33:57 +0530

    update

---
+++ lib/main_screen.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+
+class MainScreen extends StatefulWidget {
+  const MainScreen({Key? key}) : super(key: key);
+
+  @override
+  State<MainScreen> createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: WebView(
+        initialUrl: "https://www.dorpip.org.uk/",
+        javascriptMode: JavascriptMode.unrestricted,
+      ),
+    );
+  }
+}
